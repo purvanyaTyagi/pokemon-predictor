cc6b96c85870f3d383861b77e2d2ef068cb260b6
purvanyaTyagi <57037965+purvanyaTyagi@users.noreply.github.com>
2020-03-28 11:04:17 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -15,3 +15,16 @@ model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']
 model.fit(features, labels, epochs=20,batch_size = 10)
 
 model.save("pokemon.model")
+
+#----------------------------------tensorflow model---------------------
+def tensroflow_model(n_node_hl1,n_nodes_hl2,data):
+
+    dense = tf.keras.layers.Dense(500, activation = 'relu')(data)
+
+    l2 = tf.add(tf.matmul(dense,tf.random_normal([n_node_hl1, n_nodes_hl2])), tf.random_normal([n_nodes_hl2]))
+    l2 = tf.nn.relu(l2)
+
+    l3 = tf.keras.layers.Dense(500)(l2)
+    l3 = tf.nn.relu(l3)
+
+    output = tf.keras.layers.Dense(18,activation='sigmoid')(l3)
